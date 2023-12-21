e8c85e757779296dc4739f29307590401c6916cf
Edu Aguiar <137623643+eduddaguiar@users.noreply.github.com>
2023-12-21 10:51:30 -0300

    Create bloco_controller.rb

---
+++ bloco_controller.rb
@@ -0,0 +1,15 @@
+# app/modules/bloco/bloco_controller.rb
+
+class BlocoController < ApplicationController
+  def index
+    # Lógica para exibir o bloco na página inicial
+  end
+
+  def edit
+    # Lógica para exibir o painel de administração e editar o bloco
+  end
+
+  def update
+    # Lógica para salvar as alterações feitas no bloco
+  end
+end
